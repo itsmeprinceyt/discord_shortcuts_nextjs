8ba42dbc8db7deae4f8302cfdf82e88a8d42f355
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-08-29 01:31:48 +0530

    V2 with new look

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,26 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Shortcuts",
-  description: "Shortcuts that makes my life easy",
+  title: "Bookmark Shortcuts - ItsMe Prince",
+  description: "Bookmark Shortcuts that makes my life easy. These are the bookmarks which I access regularly!",
+  icons: {
+    icon: "/logo.png"
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <div className="absolute top-0 z-[-2] h-screen w-screen transform bg-gradient-to-r from-stone-900 to-sky-900">{children}</div>
+      <head>
+        <meta name="keywords" content="Mohd Uvaish, ItsMe Prince, Website, Landing Page, Product WebPage, Web Developer, Next.js, Tailwind CSS, Full Stack Developer" />
+        <meta name="author" content="Mohd Uvaish" />
+      </head>
+      <body className={`${inter.className} relative`}>
+        <div className=" bg-gradient-to-r from-stone-950 to-black">
+          {children}
+          </div>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,44 +1,83 @@
-import Card from '@/components/Card.js';
-export default function Home() {
+import { BorderBeam } from ".//(components)/magicui/border-beam.jsx";
+import Image from "next/image.js";
+import Link from "next/link.js";
+
+export default function Main() {
+  const Websites = [
+    {
+      title: "Sofi Bump",
+      src: "/static/images/Sofi.jpeg",
+      image_alt: "Sofi",
+      link: "https://sofi.gg/bump"
+    },
+    {
+      title: "Sofi",
+      src: "/static/images/Sofi.jpeg",
+      image_alt: "Sofi",
+      link: "https://top.gg/bot/853629533855809596/vote"
+    },
+    {
+      title: "Karuta",
+      src: "/static/images/karuta.png",
+      image_alt: "Sofi",
+      link: "https://top.gg/bot/646937666251915264/vote"
+    },
+    {
+      title: "Coins",
+      src: "/static/images/bot-discord-image.png",
+      image_alt: "Sofi",
+      link: "https://bot-hosting.net/panel/earn"
+    },
+    {
+      title: "Genshin Impact",
+      src: "/static/images/gi.png",
+      image_alt: "Sofi",
+      link: "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481"
+    },
+    {
+      title: "Honkai Star Rail",
+      src: "/static/images/hsr.png",
+      image_alt: "Sofi",
+      link: "https://act.hoyolab.com/bbs/event/signin/hkrpg/index.html?act_id=e202303301540311"
+    },
+  ];
   return (
-    <>
-      <div className="
-      
-      rounded-md bg-gradient-to-r from-slate-900 to-slate-700 
-      shadow-md
-      
-      
-      sticky 
-      top-[100px] 
-      m-auto 
-      grid grid-cols-4 justify-center items-center gap-[20px] p-5 
-      max-[1331px]:grid-cols-4 w-[800px] 
-      max-[1330px]:grid-cols-3 w-[550px] 
-      max-[980px]:grid-cols-2 w-[340px] 
-      max-[690px]:grid-cols-1 w-[100px]
-      
-      ">
-        <Card name="Bump"
-          image="/static/images/sofi.jpeg"
-          color="bg-pink-300"
-          url="https://sofi.gg/bump"
-        />
-        <Card name="Sofi"
-          image="/static/images/sofi.jpeg"
-          color="bg-pink-300"
-          url="https://top.gg/bot/853629533855809596/vote"
-        />
-        <Card name="Karuta"
-          image="/static/images/karuta.png"
-          color="bg-orange-300"
-          url="https://top.gg/bot/646937666251915264/vote"
-        />
-        <Card name="Coins"
-          image="/static/images/bot-discord-image.png"
-          color="bg-blue-300"
-          url="https://bot-hosting.net/panel/earn"
-        />
+    <div className="min-h-screen flex justify-center items-center overflow-hidden">
+      <div className="mt-12 mb-12 relative  bg-black rounded-xl text-white flex flex-col justify-start items-center gap-5 p-5 shadow-xl shadow-white/20 ">
+        <h1 className="font-semibold ">Bookmark Shortcuts</h1>
+        <div className="grid  grid-cols-1 md:grid-cols-2 xl:grid-cols-3 items-center gap-5">
+          {Websites.map((website, index) => (
+            <div
+              key={index}
+              className="bg-white flex justify-around items-center gap-5 text-black rounded-xl p-4 w-[250px] h-[130px]"
+            >
+              <Image
+                className="w-[100px] rounded-xl shadow-xl shadow-black/30"
+                src={website.src}
+                width={200}
+                height={200}
+                alt={website.image_alt}
+              />
+              <Link href={website.link} target="_blank">
+                <button className="bg-gradient-to-r from-black to-black/80 text-white text-xs p-3 rounded-xl shadow-xl shadow-black/30 hover:scale-105 transition-all ease-in-out w-[90px]">{website.title}</button>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className="text-[10px]">
+        Made by @itsmeprinceyt | &nbsp;
+        <Link className="hover:animate-pulse transition-all ease-in-out" href="https://github.com/itsmeprinceyt" target="_blank">
+        Github
+        </Link>
+        &nbsp; | &nbsp;
+        <Link className="hover:animate-pulse transition-all ease-in-out" href="https://www.youtube.com/@itsmeprinceyt" target="_blank">
+        YouTube
+        </Link>
+
+        
+        </div>
+        <BorderBeam />
       </div>
-    </>
+    </div>
   );
 }
